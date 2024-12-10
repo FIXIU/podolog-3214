78c53ffde0f0808fcef93a2655ae87d4c84277ce
FIXIU <fixiu@tuta.io>
2024-12-10 20:59:01 +0100

    Updated Home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,26 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'podo-1': '#010009',
+				'podo-2': '#16002a',
+				'podo-3': '#3c0055',
+				'podo-4': '#662483',
+				'podo-5': '#7e419b',
+				'podo-6': '#975db4',
+				'podo-7': '#b17acd',
+				'podo-8': '#c28ede',
+				'podo-9': '#d4a2ee',
+				'podo-10': '#e6b7ff',
+				'podo-11': '#eec2ff',
+				'podo-12': '#f6ceff',
+				'podo-13': '#fed9ff',
+				'podo-14': '#ffdfff',
+				'podo-15': '#ffe5ff',
+				'podo-16': '#ffebff',
+			},
+		},
 		fontFamily: {
 			montserrat: ['Montserrat', 'sans-serif'],
 		},
